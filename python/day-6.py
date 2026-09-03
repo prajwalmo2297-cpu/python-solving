91387304f9758e9e2941128aa29f63f1bf8b655d
prajwalmo2297-cpu <prajwalmo2297@gmail.com>
2026-09-03 20:11:34 +0530

    create day-6.py

---
+++ python/day-6.py
@@ -0,0 +1,8 @@
+#program that will tell whether the number entered by the user is odd or even.
+
+num = int(input("Enter your number:"))
+
+if num % 2 ==0:
+  print(f"{num} is even.")
+else:
+  print(f"{num} is odd.")
