aa2ef95ac00b77ade120dfd1a09aed779bc01d19
prajwalmo2297-cpu <prajwalmo2297@gmail.com>
2026-09-05 13:09:46 +0530

    Create day-17.py

---
+++ python/day-17.py
@@ -0,0 +1,6 @@
+#a program that will take three digits from the user and return the sum of the squares of each digit.
+num1 = int(input("Enter the first integer:"))
+num2 = int(input("Enter the second integer:"))
+num3 = int(input("Enter the third integer:"))
+
+print(f"the sum of squares of three integers is {(num1**2)+(num2**2)+(num3**2)}")
